e1051ae09734a9792d327e855aeb9de925e99315
Fanish Sharma <princesharma4867@gmail.com>
2025-07-19 21:23:10 +0530

    made some changes in schema.py on optional attributes default values

--- backend/schema/schema.py
+++ backend/schema/schema.py
@@ -28,9 +28,8 @@ class AddingNewEmployee(BaseModel):
     e_email:str
     e_mobile_code:Optional[str]="+91"
     e_mobile_no:str
-    e_address:Optional[str]=0
-    e_department:Optional[int]=0
-    e_joining_date:Optional[datetime]=0
+    e_address:Optional[str]="0"
+    e_department:int
     e_salary:float
     
     class Config:
@@ -48,16 +47,16 @@ class departmentResponse(BaseModel):
 
 class projectAssign(BaseModel):
     e_id:int
-    e_full_name:Optional[str]=0
+    e_full_name:Optional[str]="0"
     pj_id:int
-    pj_name:Optional[str]=0
-    pj_description:Optional[str]=0
+    pj_name:Optional[str]="0"
+    pj_description:Optional[str]="0"
     
     class Config:
         from_attributes=True
         
 class AddingNewProject(BaseModel):
-    pj_name:Optional[str]=0
+    pj_name:Optional[str]='0'
     pj_description:Optional[str]=pj_name
     
     class Config:
@@ -79,22 +78,22 @@ class checkOut(BaseModel):
         
 class updateResponse(BaseModel):
     e_id:Optional[int]=0
-    e_name:Optional[str]=0
-    e_address:Optional[str]=0
+    e_name:Optional[str]="0"
+    e_address:Optional[str]="0"
     e_phone_code:Optional[str]='+91'
-    e_email:Optional[str]=0
-    e_phone_no:Optional[str]=0
+    e_email:Optional[str]="0"
+    e_phone_no:Optional[str]='0'
     e_department:Optional[int]=0
     
-    d_id:Optional[str]=0
-    d_name:Optional[str]=0
-    d_description:Optional[str]=0
-    d_location:Optional[str]=0
+    d_id:Optional[int]=0
+    d_name:Optional[str]='0'
+    d_description:Optional[str]="0"
+    d_location:Optional[str]="0"
     Salary_Before_Deduction:Optional[float]=0
     
     pj_id:Optional[int]=0
-    pj_name:Optional[str]=0
-    pj_description:Optional[str]=0
+    pj_name:Optional[str]="0"
+    pj_description:Optional[str]="0"
     
     table_name:str
     class Config:
